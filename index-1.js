bf00d28e4c5dae10f8e4ffed4d3bf074081f8f28
Akshay Rathee <akshayrathee902@gmail.com>
2024-07-24 00:34:50 +0530

    added ques

--- index-1.js
+++ index-1.js
@@ -100,3 +100,101 @@ arr.forEach(function (elem) {
 });
 
 console.log(sum);
+
+/*in an array of no and str only add those which are not str */
+var arrNum = [1, 2, "asdasd", 4, "rty", 5, 345];
+var sum = 0;
+
+arrNum.forEach(function (ele) {
+  if (typeof ele === "number") {
+    sum = sum + ele;
+  }
+});
+console.log(sum);
+
+/* remove gender != male*/
+var arrObj = [
+  { name: "harsh", gender: "male" },
+  { name: "harshita", gender: "female" },
+  { name: "ram", gender: "male" },
+  { name: "ankita", gender: "female" },
+  { name: "shruti", gender: "female" },
+];
+
+// var newarr = arr.filter(function (ele) {
+//   return ele.gender === "male";
+// });
+
+var count = 0;
+arrObj.forEach(function (ele) {
+  if (ele.gender !== "male") count++;
+});
+//cannot break forEach loop so we use for loop
+for (var j = 0; j <= count; j++) {
+  for (var i = 0; i < arrObj.length; i++) {
+    if (arrObj[i].gender !== "male") {
+      arrObj.splice(i, 1);
+    }
+  }
+}
+
+console.log(arrObj);
+
+/*clone array */
+
+function cloneArr(arr) {
+  return [...arr];
+}
+var ansArr = cloneArr([1, 2, 3, 4, 5]);
+console.log(ansArr);
+
+/*tell type */
+function typeTeller(elem) {
+  return typeof elem;
+}
+
+console.log(typeTeller([]));
+console.log(typeTeller(12));
+console.log(typeTeller("str"));
+
+/* get first element of array. passing 'n' will return first 'n' elements*/
+
+function retrieve(arr, n = 1) {
+  if (n <= arr.length) {
+    for (var i = 0; i < n; i++) {
+      console.log(arr[i]);
+    }
+  } else console.log("not this much elements");
+}
+
+retrieve([1, 2, 3, 4, 5, 6, 7, 8]);
+retrieve([1, 2, 3, 4, 5, 6, 7, 8], 5);
+retrieve([1, 2, 3, 4, 5], 6);
+
+/* get last element of array. passing 'n' will return last 'n' elements*/
+
+function retrieveLast(arr, n = 1) {
+  if (n <= arr.length) {
+    for (var i = 0; i < n; i++) {
+      console.log(arr[arr.length - 1 - i]);
+    }
+  } else console.log("not this much elements");
+}
+
+retrieveLast([1, 2, 3, 4, 5, 6, 7, 8]);
+retrieveLast([1, 2, 3, 4, 5, 6, 7, 8], 5);
+retrieveLast([1, 2, 3, 4, 5], 6);
+
+/*find most frq element of array */
+function freq(arr) {
+  var freq = {};
+  arr.forEach(function (elem) {
+    if (freq.hasOwnProperty(elem)) freq[elem]++;
+    else freq[elem] = 1;
+  });
+  var ans = Object.keys(freq).reduce(function (acc, num) {
+    return freq[acc] > freq[num] ? acc : num;
+  });
+  console.log(ans);
+}
+freq([1, 1, 1, 1, 2, 2, 2, 4, 4, 5, 5, 6, 7, 7, 8, 8]);
